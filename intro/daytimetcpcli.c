bda8d8ddfcd593f8958648eaddb6f887d108b929
czw315 <1348848331@qq.com>
2020-09-05 18:01:41 +0800

    page two

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -13,7 +13,7 @@ int main(int argc, char** argv){
     
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family=AF_INET;
-    servaddr.sin_port=htons(9999);
+    servaddr.sin_port=htons(13);
     if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr)<=0)
         err_quit("inet_pton error");
     
